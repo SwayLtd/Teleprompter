c802e3f6d14c4d25dddc37be4e1cea14192b3d44
Sioxox <sioxox@pm.me>
2023-05-01 12:40:25 +0200

    Moving CSS and Javascript in dedicated files

---
+++ static/js/main.js
@@ -0,0 +1,401 @@
+const socket = io.connect('http://' + document.domain + ':' + location.port);
+let isPlaying = false;
+let autoScrollInterval;
+
+socket.on('connect', () => {
+    console.log('Connected');
+});
+
+socket.on('update_text', data => {
+    $('#sync-text').html(data['text']);
+    $('#sync-text').css('font-size', data['fontSize'] + 'px');
+    $('#speed').val(data['speed'] * 10);
+    $('#sync-text').scrollTop(data['scrollTop']);
+    isPlaying = data['isPlaying'];
+    $('#play-pause').html(isPlaying ? '<i class="fas fa-pause"></i>' : '<i class="fas fa-play"></i>');
+    if (isPlaying) {
+        clearInterval(autoScrollInterval);
+        autoScrollInterval = setInterval(autoScroll, 25);
+    } else {
+        clearInterval(autoScrollInterval);
+        autoScrollInterval = null;
+    }
+    $('#font-size-value').text(data['fontSize'] + 'px');
+    $('#font-size').val(data['fontSize']);
+    $('#reading-speed-value').text(getSpeed() * 100 + '%');
+});
+
+
+$('#save-text').click(() => {
+    let text = $('#sync-text').html();
+    let fontSize = parseInt($('#sync-text').css('font-size'));
+    socket.emit('text_updated', { 'text': text, 'fontSize': fontSize, scrollTop: $('#sync-text').scrollTop(), 'isPlaying': isPlaying, 'speed': getSpeed() });
+    saveState();
+});
+
+$('#font-size, #speed').on('input', () => {
+    let fontSize = $('#font-size').val();
+    socket.emit('text_updated', { 'text': $('#sync-text').html(), 'fontSize': fontSize, scrollTop: $('#sync-text').scrollTop(), 'isPlaying': isPlaying, 'speed': getSpeed() });
+    saveState();
+});
+
+$('#play-pause').on('click', () => {
+    isPlaying = !isPlaying;
+    let fontSize = parseInt($('#sync-text').css('font-size'));
+    $('#play-pause').html(isPlaying ? '<i class="fas fa-pause"></i>' : '<i class="fas fa-play"></i>');
+    socket.emit('text_updated', { 'text': $('#sync-text').html(), 'fontSize': fontSize, scrollTop: $('#sync-text').scrollTop(), 'isPlaying': isPlaying, 'speed': getSpeed() });
+    if (isPlaying) {
+        clearInterval(autoScrollInterval);
+        autoScroll();
+    } else {
+        clearInterval(autoScrollInterval);
+        autoScrollInterval = null;
+    }
+    saveState();
+});
+
+$('#text-color').on('input', () => {
+    const textColor = $('#text-color').val();
+    $('body').css('color', textColor);
+    $('#sync-text').css('color', textColor);
+    localStorage.setItem('textColor', textColor);
+});
+
+$('#bg-color').on('input', () => {
+    const bgColor = $('#bg-color').val();
+    $('body').css('background-color', bgColor);
+    localStorage.setItem('bgColor', bgColor);
+});
+
+$('#toggle-align').click(() => {
+    const textAlign = $('#sync-text').css('text-align');
+
+    let newTextAlign;
+    let iconClass;
+
+    if (textAlign === 'left') {
+        newTextAlign = 'center';
+        iconClass = 'fas fa-align-center';
+    } else if (textAlign === 'center') {
+        newTextAlign = 'right';
+        iconClass = 'fas fa-align-right';
+    } else if (textAlign === 'right') {
+        newTextAlign = 'left';
+        iconClass = 'fas fa-align-left';
+    }
+
+    $('#sync-text').css('text-align', newTextAlign);
+    $('#toggle-align i').attr('class', iconClass);
+
+    // save local state of the text alignment to the local browser storage
+    localStorage.setItem('textAlign', newTextAlign);
+});
+
+$('#invert-v').click(() => {
+    let scaleY = parseFloat($('#sync-text').css('transform').split(',')[3]);
+    let scaleX = parseFloat($('#sync-text').css('transform').split(',')[0].split('(')[1]);
+    scaleY = scaleY === 1 ? -1 : 1;
+    $('#sync-text').css('transform', `scaleX(${scaleX}) scaleY(${scaleY})`);
+
+    localStorage.setItem('textOrientation', `scaleX(${scaleX}) scaleY(${scaleY})`);
+});
+
+$('#invert-h').click(() => {
+    let scaleY = parseFloat($('#sync-text').css('transform').split(',')[3]);
+    let scaleX = parseFloat($('#sync-text').css('transform').split(',')[0].split('(')[1]);
+    scaleX = scaleX === 1 ? -1 : 1;
+    $('#sync-text').css('transform', `scaleX(${scaleX}) scaleY(${scaleY})`);
+
+    localStorage.setItem('textOrientation', `scaleX(${scaleX}) scaleY(${scaleY})`);
+});
+
+$('#reset').click(() => {
+    $('#sync-text').css('transform', 'scaleX(1) scaleY(1)');
+    $('#sync-text').css('text-align', 'center');
+    let fontSize = $('#font-size').val();
+    $('#toggle-align i').attr('class', 'fas fa-align-center');
+    // text-color to #61dafb and background-color to #282c34
+    $('#text-color').val('#61dafb');
+    $('#bg-color').val('#282c34');
+    $('#sync-text').css('color', '#61dafb');
+    $('body').css('color', '#61dafb');
+    $('body').css('background-color', '#282c34');
+    // reset sync text width and arrows position
+    $('#sync-text-width').val(90);
+    $('#sync-text').width('90%');
+    $('.arrow-left').css('right', '0');
+    $('.arrow-right').css('left', '0');
+    $('#sync-text-width-value').text('90%');
+    localStorage.clear();
+    socket.emit('text_updated', { 'text': $('#sync-text').html(), 'fontSize': fontSize, scrollTop: $('#sync-text').scrollTop(), 'isPlaying': isPlaying, 'speed': getSpeed() });
+    saveState();
+});
+
+$('#font-size').on('input', () => {
+    let fontSize = $('#font-size').val();
+    $('#font-size-value').text(fontSize);
+    $('#sync-text').css('font-size', fontSize + 'px');
+});
+
+$('#speed').on('input', () => {
+    $('#reading-speed-value').text(getSpeed() * 100 + '%');
+});
+
+$('#sync-text-width').on('input', function () {
+    const syncText = $('#sync-text');
+    syncText.width($(this).val() + '%');
+    const arrowPosition = ((100 - parseInt($(this).val())) / 2) - 5;
+
+    // Change the arrow positions
+    $('.arrow-left').css('right', arrowPosition + '%');
+    $('.arrow-right').css('left', arrowPosition + '%');
+
+    $('#sync-text-width-value').text($(this).val() + '%');
+
+    localStorage.setItem('syncTextWidth', $(this).val());
+});
+
+// Shortcuts
+window.addEventListener('keydown', function (e) {
+    const syncTextArea = document.querySelector('#sync-text');
+
+    // Ignore keydown events if the target is the #sync-text area
+    if (e.target === syncTextArea) {
+        return;
+    }
+    if (e.key === ' ' || e.key === 'p' || e.key === 'P') {
+        // Toggle play/pause
+        isPlaying = !isPlaying;
+        let fontSize = parseInt($('#sync-text').css('font-size'));
+        $('#play-pause').html(isPlaying ? '<i class="fas fa-pause"></i>' : '<i class="fas fa-play"></i>');
+        socket.emit('text_updated', { 'text': $('#sync-text').html(), 'fontSize': fontSize, scrollTop: $('#sync-text').scrollTop(), 'isPlaying': isPlaying, 'speed': getSpeed() });
+        if (isPlaying) {
+            clearInterval(autoScrollInterval);
+            autoScroll();
+        } else {
+            clearInterval(autoScrollInterval);
+            autoScrollInterval = null;
+        }
+        saveState();
+    } else if (e.key === 'ArrowUp' || e.key === 'w' || e.key === 'W') {
+        // Increase reading speed
+        let speed = getSpeed();
+        if (speed < 1) {
+            speed += 0.1;
+            let fontSize = parseInt($('#sync-text').css('font-size'));
+            $('#speed').val(speed * 10);
+            $('#reading-speed-value').text(speed * 100 + '%');
+            socket.emit('text_updated', { 'text': $('#sync-text').html(), 'fontSize': fontSize, scrollTop: $('#sync-text').scrollTop(), 'isPlaying': isPlaying, 'speed': getSpeed() });
+            saveState();
+        }
+    } else if (e.key === 'ArrowDown' || e.key === 's' || e.key === 'S') {
+        // Decrease reading speed
+        let speed = getSpeed();
+        if (speed > 0.1) {
+            speed -= 0.1;
+            let fontSize = parseInt($('#sync-text').css('font-size'));
+            $('#speed').val(speed * 10);
+            $('#reading-speed-value').text(speed * 100 + '%');
+            socket.emit('text_updated', { 'text': $('#sync-text').html(), 'fontSize': fontSize, scrollTop: $('#sync-text').scrollTop(), 'isPlaying': isPlaying, 'speed': getSpeed() });
+            saveState();
+        }
+    } // increase font size
+    else if (e.key === 'ArrowRight' || e.key === 'd' || e.key === 'D') {
+        let fontSize = parseInt($('#sync-text').css('font-size'));
+        fontSize += 1;
+        $('#font-size').val(fontSize);
+        $('#font-size-value').text(fontSize + 'px');
+        socket.emit('text_updated', { 'text': $('#sync-text').html(), 'fontSize': fontSize, scrollTop: $('#sync-text').scrollTop(), 'isPlaying': isPlaying, 'speed': getSpeed() });
+        saveState();
+    } // decrease font size
+    else if (e.key === 'ArrowLeft') {
+        let fontSize = parseInt($('#sync-text').css('font-size'));
+        fontSize -= 1;
+        $('#font-size').val(fontSize);
+        $('#font-size-value').text(fontSize + 'px');
+        socket.emit('text_updated', { 'text': $('#sync-text').html(), 'fontSize': fontSize, scrollTop: $('#sync-text').scrollTop(), 'isPlaying': isPlaying, 'speed': getSpeed() });
+        saveState();
+    } // increase font size
+    else if (e.key === 'ArrowRight') {
+        let fontSize = parseInt($('#sync-text').css('font-size'));
+        fontSize += 1;
+        $('#font-size').val(fontSize);
+        $('#font-size-value').text(fontSize + 'px');
+        socket.emit('text_updated', { 'text': $('#sync-text').html(), 'fontSize': fontSize, scrollTop: $('#sync-text').scrollTop(), 'isPlaying': isPlaying, 'speed': getSpeed() });
+        saveState();
+    }
+});
+
+window.addEventListener('wheel', (e) => {
+    if (e.altKey) {
+        if (e.deltaY > 0) {
+            // decrease reading speed
+            let speed = getSpeed();
+            if (speed > 0.1) {
+                speed -= 0.1;
+                let fontSize = parseInt($('#sync-text').css('font-size'));
+                $('#speed').val(speed * 10);
+                $('#reading-speed-value').text(speed * 100 + '%');
+                socket.emit('text_updated', { 'text': $('#sync-text').html(), 'fontSize': fontSize, scrollTop: $('#sync-text').scrollTop(), 'isPlaying': isPlaying, 'speed': getSpeed() });
+                saveState();
+            }
+        } else {
+            // increase reading speed
+            let speed = getSpeed();
+            if (speed < 1) {
+                speed += 0.1;
+                let fontSize = parseInt($('#sync-text').css('font-size'));
+                $('#speed').val(speed * 10);
+                $('#reading-speed-value').text(speed * 100 + '%');
+                socket.emit('text_updated', { 'text': $('#sync-text').html(), 'fontSize': fontSize, scrollTop: $('#sync-text').scrollTop(), 'isPlaying': isPlaying, 'speed': getSpeed() });
+                saveState();
+            }
+        }
+    } else if (e.shiftKey) {
+        if (e.deltaY > 0) {
+            // decrease font size
+            let fontSize = parseInt($('#sync-text').css('font-size'));
+            if (fontSize > 45) {
+                fontSize -= 1;
+                $('#font-size').val(fontSize);
+                $('#font-size-value').text(fontSize + 'px');
+                socket.emit('text_updated', { 'text': $('#sync-text').html(), 'fontSize': fontSize, scrollTop: $('#sync-text').scrollTop(), 'isPlaying': isPlaying, 'speed': getSpeed() });
+                saveState();
+            }
+        } else {
+            // increase font size
+            let fontSize = parseInt($('#sync-text').css('font-size'));
+            if (fontSize < 72) {
+                fontSize += 1;
+                $('#font-size').val(fontSize);
+                $('#font-size-value').text(fontSize + 'px');
+                socket.emit('text_updated', { 'text': $('#sync-text').html(), 'fontSize': fontSize, scrollTop: $('#sync-text').scrollTop(), 'isPlaying': isPlaying, 'speed': getSpeed() });
+                saveState();
+            }
+        }
+    }
+});
+
+function getSpeed() {
+    return parseFloat($('#speed').val()) / 10;
+}
+
+function autoScroll() {
+    const speed = getSpeed();
+    const fontSize = parseInt($('#sync-text').css('font-size'));
+    const scrollTop = $('#sync-text').scrollTop();
+    $('#sync-text').scrollTop(scrollTop + speed * fontSize / 5.62);
+}
+
+function saveState() {
+    const state = {
+        fontSize: $('#font-size').val(),
+        speed: getSpeed(),
+        isPlaying: isPlaying,
+        text: $('#sync-text').html(),
+        scrollTop: $('#sync-text').scrollTop()
+    };
+
+    $.ajax({
+        url: '/save_state',
+        method: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(state),
+        success: function (response) {
+            console.log('State saved successfully:', response);
+        },
+        error: function (error) {
+            console.error('Error saving state:', error);
+        }
+    });
+}
+
+function loadState() {
+    $.ajax({
+        url: '/load_state',
+        method: 'GET',
+        success: function (state) {
+            if (state) {
+                $('#sync-text').html(state.text || '\n\n\n\n\n\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\n\n\n\n\n\n');
+                $('#sync-text').css('font-size', (state.fontSize || 45) + 'px');
+                $('#font-size-value').text((state.fontSize || 45) + 'px');
+                $('#font-size').val(state.fontSize || 45);
+                $('#speed').val(((state.speed || 0.1) * 10));
+                $('#reading-speed-value').text((state.speed || 0.1) * 100 + '%');
+                $('#sync-text').scrollTop(state.scrollTop + 200 || 0); // + 200 is needed?
+                isPlaying = (typeof state.isPlaying !== 'undefined') ? state.isPlaying : false;
+                $('#play-pause').html(isPlaying ? '<i class="fas fa-pause"></i>' : '<i class="fas fa-play"></i>');
+                if (isPlaying) {
+                    clearInterval(autoScrollInterval);
+                    autoScrollInterval = setInterval(autoScroll, 25);
+                } else {
+                    clearInterval(autoScrollInterval);
+                    autoScrollInterval = null;
+                }
+            } else {
+                $('#sync-text').html('\n\n\n\n\n\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\n\n\n\n\n\n');
+                $('#sync-text').css('font-size', '45px');
+                $('#font-size-value').text('45px');
+                $('#font-size').val(45);
+                $('#speed').val(1); // 0.3 * 10
+                $('#reading-speed-value').text(10 + '%');
+                $('#sync-text').scrollTop(0);
+                isPlaying = false;
+                $('#play-pause').html('<i class="fas fa-play"></i>');
+            }
+        },
+        error: function (error) {
+            console.error('Error loading state:', error);
+        }
+    });
+}
+
+function loadLocalStorage() {
+    const savedTextColor = localStorage.getItem('textColor');
+    const savedBgColor = localStorage.getItem('bgColor');
+    const savedTextAlign = localStorage.getItem('textAlign');
+    const savedTextOrientation = localStorage.getItem('textOrientation');
+    const savedSyncTextWidth = localStorage.getItem('syncTextWidth');
+
+    if (savedTextColor) {
+        $('#text-color').val(savedTextColor);
+        $('#sync-text').css('color', savedTextColor);
+        $('body').css('color', savedTextColor);
+    }
+
+    if (savedBgColor) {
+        $('#bg-color').val(savedBgColor);
+        $('body').css('background-color', savedBgColor);
+    }
+
+    if (savedTextAlign) {
+        $('#sync-text').css('text-align', savedTextAlign);
+        iconClass = 'fas fa-align-' + savedTextAlign;
+        $('#toggle-align i').attr('class', iconClass);
+    }
+
+    if (savedTextOrientation) {
+        $('#sync-text').css('transform', savedTextOrientation);
+    }
+
+    if (savedSyncTextWidth) {
+        const syncText = $('#sync-text');
+        syncText.width(savedSyncTextWidth + '%');
+        const arrowPosition = ((100 - parseInt(savedSyncTextWidth)) / 2) - 5;
+
+        // Change the arrow positions
+        $('.arrow-left').css('right', arrowPosition + '%');
+        $('.arrow-right').css('left', arrowPosition + '%');
+        $('#sync-text-width').val(savedSyncTextWidth);
+        $('#sync-text-width-value').text(savedSyncTextWidth + '%');
+    }
+}
+
+// Initialize font size and reading speed labels
+$('#font-size-value').text($('#font-size').val() + 'px');
+$('#reading-speed-value').text(getSpeed() * 100 + '%');
+$('#sync-text-width-value').text($('#sync-text-width').val() + '%');
+
+// Initialize settings
+loadState(); // Load state from the server
+loadLocalStorage(); // Load state from the local browser storage
